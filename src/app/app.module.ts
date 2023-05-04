2892c43f8c1a4b635ab12c115f4f948e1e09ff5c
MichaelAI0 <99698667+MichaelAI0@users.noreply.github.com>
2023-05-04 01:50:09 -0500

    created a material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
 import { TeamListContainerComponent } from './team-list-container/team-list-container.component';
-import {MatDividerModule} from '@angular/material/divider';
-
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TeamListContainerComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatInputModule,
-    MatCardModule,
-    MatDividerModule
-  ],
+  declarations: [AppComponent, HeaderComponent, TeamListContainerComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
