9061455ce9810041895364a3a3cf27f85c6ba705
Jennifer Lopez <111323297+thecakemom@users.noreply.github.com>
2023-05-14 14:45:14 -0500

    List members of team in expansion panels

--- src/app/team-expansion-panel/team-expansion-panel.component.ts
+++ src/app/team-expansion-panel/team-expansion-panel.component.ts
@@ -15,4 +15,30 @@ export class TeamExpansionPanelComponent {
     'Purple Parrots',
     'Silver Snakes',
   ];
+
+  defaultPhoto: string = '/assets/images/avatar.png';
+
+  members: member[] = [
+    { name: 'James Halfhill', photo: '' },
+    { name: 'Devin Gamestop', photo: '/assets/images/pexels-dominika-roseclay-977878.jpg' },
+    { name: 'Aosu Yakoma', photo: '/assets/images/pexels-fauxels-3183198.jpg' },
+    { name: 'Tammy Panel', photo: '/assets/images/pexels-igor-haritanovich-1695052.jpg' },
+    { name: 'Jane Smith', photo: '/assets/images/pexels-moose-photos-1037992.jpg' },
+    { name: 'Lucille Ball', photo: '/assets/images/pexels-philippe-donn-1114690.jpg' },
+    { name: 'Desi Arnaz', photo: '/assets/images/pexels-pixabay-259915.jpg' },
+    { name: 'Kim Danger', photo: '/assets/images/pexels-pixabay-461940.jpg' },
+    { name: 'Apple Coldplay', photo: '/assets/images/pexels-polina-tankilevitch-4110008.jpg' },
+    { name: 'Moses Bean', photo: '/assets/images/pexels-scott-webb-28080.jpg' },
+    { name: 'Bethany Culver', photo: '/assets/images/pexels-simon-berger-1323550.jpg' },
+    { name: 'Robert Bob', photo: '/assets/images/pexels-tan-danh-1329711.jpg' },
+    // Add more member objects here
+  ];
+  getMemberPhoto(member: member): string {
+    return member.photo || this.defaultPhoto;
+  }
+}
+
+interface member {
+  photo: string | null;
+  name: string;
 }
