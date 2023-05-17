13328f7ef71df309055fa2a46cbb6d219419451f
MichaelAI0 <99698667+MichaelAI0@users.noreply.github.com>
2023-05-16 23:45:46 -0500

    fix team dialog

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeamService {
-  private apiUrl = ''; //TODO: Replace with your Rails API URL
+  private baseApiUrl = environment.apiUrl;
+  private apiTeamUrl = `${this.baseApiUrl}/teams`;
   constructor(private http: HttpClient) {}
 
   getTeam(id: number) {
-    return this.http.get(`${this.apiUrl}/${id}`);
+    return this.http.get(`${this.apiTeamUrl}/${id}`);
+  }
+  getAllTeams(): Observable<any> {
+    return this.http.get(`${this.apiTeamUrl}`);
   }
 }
--- src/app/team-expansion-panel/team-expansion-panel.component.ts
+++ src/app/team-expansion-panel/team-expansion-panel.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TeamService } from 'src/app/services/team.service';
 @Component({
   selector: 'app-team-expansion-panel',
   templateUrl: './team-expansion-panel.component.html',
   styleUrls: ['./team-expansion-panel.component.scss'],
 })
-export class TeamExpansionPanelComponent {
+export class TeamExpansionPanelComponent implements OnInit {
+  ngOnInit(): void {
+    this.showTeams();
+  }
   // TODO: Get Team names from backend
-  list: string[] = [
-    'Red Jaguars',
-    'Blue Barracudas',
-    'Green Monkeys',
-    'Orange Iguanas',
-    'Purple Parrots',
-    'Silver Snakes',
-  ];
+  list: string[] = [];
+
+  constructor(private team: TeamService) {}
+
+  showTeams() {
+    return this.team.getAllTeams().subscribe((res) => {
+      console.log(res);
+      this.list = res;
+    });
+  }
 }
